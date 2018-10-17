b7726ea2ff7603962e07a3259ed0cb15ac66d21c
jlxy04 <jlxy04@gmail.com>
2018-10-17 16:35:22 +0800

    init

--- demo-api/src/main/java/com/yifeng/demo/boss/api/user/service/UserService.java
+++ demo-api/src/main/java/com/yifeng/demo/boss/api/user/service/UserService.java
@@ -3,12 +3,32 @@
  */
 package com.yifeng.demo.boss.api.user.service;
 
+import java.util.List;
+
+import com.yifeng.demo.boss.api.user.model.UserModel;
+import com.yifeng.demo.boss.api.user.query.UserQuery;
+
+import next.rapid.page.Pagination;
+
 /**
  * 用户相关服务
  * @author Administrator
  *
  */
 public interface UserService {
-
 	
+	/**
+	 * 根据条件分页查询
+	 * @param userQuery 查询条件
+	 * @param pagination 分页条件
+	 * @return 返回结果用户列表
+	 */
+	public List<UserModel> listUserByPage(UserQuery userQuery, Pagination pagination);
+	
+	/**
+	 * 根据用户ID查询单个用户信息
+	 * @param id 用户ID
+	 * @return 返回用户详情
+	 */
+	public UserModel getUserById(String id);
 }
--- demo-service/src/main/java/com/yifeng/demo/boss/service/user/UserServiceImpl.java
+++ demo-service/src/main/java/com/yifeng/demo/boss/service/user/UserServiceImpl.java
@@ -1,14 +1,41 @@
 package com.yifeng.demo.boss.service.user;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
+import com.yifeng.demo.boss.api.user.model.UserModel;
+import com.yifeng.demo.boss.api.user.query.UserQuery;
 import com.yifeng.demo.boss.api.user.service.UserService;
+import com.yifeng.demo.boss.dal.user.dao.UserDao;
+import com.yifeng.demo.boss.dal.user.entity.UserEntity;
 
 import next.rapid.RapidService;
+import next.rapid.page.Pagination;
 
 /**
  * 用户相关服务的实现
  * @author Administrator
  *
  */
+@Service("/boss/userService")
 public class UserServiceImpl extends RapidService implements UserService {
-
+	
+	@Autowired
+	private UserDao userDao;
+	
+	@Override
+	public List<UserModel> listUserByPage(UserQuery userQuery, Pagination pagination) {
+		PageHelper.startPage(pagination);
+		Page<UserEntity> userList = (Page<UserEntity>) userDao.selectByPage(copy(userQuery, com.yifeng.demo.boss.dal.user.query.UserQuery.class));
+		return copyList(userList, UserModel.class);
+	}
+	
+	@Override
+	public UserModel getUserById(String id) {
+		return copy(userDao.selectById(id), UserModel.class);
+	}
 }
